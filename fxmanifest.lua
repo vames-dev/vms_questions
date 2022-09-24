d630a8766944d102d4a723bdee603371f85fc21b
vames-dev <60104107+vames-dev@users.noreply.github.com>
2022-09-24 17:31:50 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+lua54 'yes'
+game "gta5"
+
+description "vms_questions"
+author "vames™️#1400"
+version '1.0.0'
+
+client_script "client/client.lua"
+
+ui_page 'html/ui.html'
+files {
+  'html/*.*',
+}
